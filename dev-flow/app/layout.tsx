8ed858fad43d895e03099626a86c54411f085266
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2023-11-25 21:14:33 +0100

    feat(frontend): adding layouts for the appearance of the application
    Co-Authored-By: Andronache George Eduard <42812475+neutralfusion@users.noreply.github.com>

--- dev-flow/app/layout.tsx
+++ dev-flow/app/layout.tsx
@@ -1,5 +1,29 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Dev Flow",
+  description:
+    "Dev Flow is a collaborative online platform designed to empower individuals to share and expand their knowledge in a wide range of topics. Much like Stack Overflow, Dev Flow is a community-driven Q&A platform where users can ask questions, provide answers, and engage in discussions to foster a rich exchange of information.",
+  icons: {
+    icon: "assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
