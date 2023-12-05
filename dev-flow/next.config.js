7b48b466d40e935a127f703376e07716a6ccee19
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2023-12-05 01:01:11 +0100

    chore: upgrade package
    Co-Authored-By: Andronache George Eduard <42812475+neutralfusion@users.noreply.github.com>

--- dev-flow/next.config.js
+++ dev-flow/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     mdxRs: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
+  typescript: { ignoreBuildErrors: true },
   images: {
     remotePatterns: [
       {
